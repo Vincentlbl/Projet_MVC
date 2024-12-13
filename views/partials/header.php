a569c58a1bfb32593d6141d7ba153cdbe3ae0004
Vincent <vlebel94@gmail.com>
2024-12-13 09:14:08 +0100

    Ajout des fichiers d'en-tête et de pied de page

---
+++ views/partials/header.php
@@ -0,0 +1,15 @@
+
+<header class="bg-gray-800 text-white p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+            <a href="/smartbike_project/views/home.php" class="text-lg font-bold flex items-center">
+                <img src="/smartbike_project/public/assets/images/logo.jpg" alt="Smartbike" class="h-12">
+            </a>
+        </div>
+        <nav>
+            <a href="/smartbike_project/views/home.php" class="text-white hover:underline mx-2">Accueil</a>
+            <a href="/smartbike_project/views/velos.php" class="text-white hover:underline mx-2">Vélos</a>
+            <a href="/smartbike_project/views/contact.php" class="text-white hover:underline mx-2">Contact</a>
+        </nav>
+    </div>
+</header>
